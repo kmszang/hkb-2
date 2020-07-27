f50ae0caa7265adb03f40c4358d99cf0cf99b58f
naamoonoo <naamoonoo.y@gmail.com>
2020-07-27 16:50:18 +0900

    [#1] env: babel setup

---
+++ client/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function (api) {
+  api.cache(true)
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        targets: '>= 1%, not dead',
+        useBuiltIns: 'usage',
+        corejs: 3,
+        modules: false,
+      },
+    ],
+    '@babel/preset-typescript',
+  ]
+
+  return { presets }
+}
