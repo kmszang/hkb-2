0a501b513f9734fe51df38aa4d82c363468c016e
andy nam <naamoonoo.y@gmail.com>
2020-07-28 11:23:36 +0900

    [#11] evn: jest setup

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
-	moduleNameMapper: {
-		"@/(.*)": "<rootDir>/src/$1",
-	},
-	modulePathIgnorePatterns: ["dist"],
+	testMatch: [
+		"**/__tests__/**/*.[jt]s?(x)",
+		"**/?(*.)+(spec|test).[tj]s?(x)",
+	],
 };
