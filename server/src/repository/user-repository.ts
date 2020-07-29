4a26eff955621bbde482faaf4ae25d9da5791220
andy nam <naamoonoo.y@gmail.com>
2020-07-29 16:07:22 +0900

    [#13] feat: design respository with user sample

---
+++ server/src/repository/user-repository.ts
@@ -0,0 +1,36 @@
+import {
+	insertQueryExecuter,
+	selectQueryExecuter,
+} from "../utils/query-executor";
+
+export interface IUser {
+	id: number;
+	name: string;
+	user_id: string;
+	social_id: string;
+	password: string;
+}
+
+export interface ISignUpBody {
+	name: string;
+	userId: string;
+	password: string;
+}
+
+export interface ISocialLoginBody {
+	socialId: string;
+}
+
+export class User {
+	static async createWithId(args: ISignUpBody) {
+		const { name, userId, password } = args;
+		const userCreateQuery = `INSERT INTO User(name, user_id, password) VALUES("${name}", "${userId})", "${password}")`;
+
+		return await insertQueryExecuter(userCreateQuery);
+	}
+
+	static async createWithSocial() {
+		const selecteAllUserQuery = ``;
+		return await selectQueryExecuter<IUser>(selecteAllUserQuery);
+	}
+}
