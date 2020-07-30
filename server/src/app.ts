64eaac2159c789df81c46d99754b1c29770a20fe
parkjihwanjay <cc6656@naver.com>
2020-07-30 17:38:34 +0900

    파일명 수정에 딷른 import문 변경

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import router from "./routes";
 import path from "path";
 import cors from "cors";
-import { CustomError } from "./errors/customError";
+import { CustomError } from "./errors/custom-error";
 import passport from "./passport";
 import session from "express-session";
 const app = express();
