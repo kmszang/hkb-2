b5808991b5bb7ae5be6b1f4d98caf20afd8eb94a
andy nam <naamoonoo.y@gmail.com>
2020-07-30 16:47:25 +0900

    [#39] feat: setup transaction fetch request api

---
+++ client/src/api/transaction.ts
@@ -0,0 +1,34 @@
+import { fetchWrapper } from '../utils/fetchWrapper'
+import { TRANSACTION } from './apiRoutes'
+
+export interface ICreateTransaction {
+  content: string
+  price: number
+  paymentId: number
+  userId: number
+  categoryId: number
+}
+
+export interface ITransactionResponse {
+  id: number
+  content: string
+  price: number
+  paymentName: string
+  categoryName: string
+  createdAt: Date
+}
+
+export const fetchAllTransaction = async () => {
+  return await fetchWrapper<ITransactionResponse[], undefined>(
+    'GET',
+    TRANSACTION
+  )
+}
+
+export const createNewTransaction = async (args: ICreateTransaction) => {
+  return await fetchWrapper<ITransactionResponse[], ICreateTransaction>(
+    'POST',
+    TRANSACTION,
+    args
+  )
+}
