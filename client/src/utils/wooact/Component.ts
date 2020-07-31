3faa438381bb4634675f493571292cbaef2107f5
parkjihwanjay <cc6656@naver.com>
2020-07-31 16:04:22 +0900

    prettier tab 조절

--- client/src/utils/wooact/Component.ts
+++ client/src/utils/wooact/Component.ts
@@ -1,86 +1,86 @@
-import { Diffing } from "./Diffing";
+import { Diffing } from './Diffing'
 
 abstract class Component<P, S> {
-	protected element: HTMLElement;
+  protected element: HTMLElement
 
-	constructor(public props?: P, private state?: S) {
-		Object.setPrototypeOf(this, Component.prototype);
-	}
+  constructor(public props?: P, private state?: S) {
+    Object.setPrototypeOf(this, Component.prototype)
+  }
 
-	getElement(): HTMLElement {
-		return this.element;
-	}
+  getElement(): HTMLElement {
+    return this.element
+  }
 
-	unmount() {
-		this.comopnentWillUnmount();
-		this.element.remove();
-	}
+  unmount() {
+    this.comopnentWillUnmount()
+    this.element.remove()
+  }
 
-	private reRender() {
-		const diffUpdatedElement = Diffing.getNewlyRendered(
-			this.element,
-			this.render()
-		);
+  private reRender() {
+    const diffUpdatedElement = Diffing.getNewlyRendered(
+      this.element,
+      this.render()
+    )
 
-		if (this.element === diffUpdatedElement) {
-			return;
-		}
-		const oldElement = this.element;
-		this.element = diffUpdatedElement;
-		oldElement.remove();
-	}
+    if (this.element === diffUpdatedElement) {
+      return
+    }
+    const oldElement = this.element
+    this.element = diffUpdatedElement
+    oldElement.remove()
+  }
 
-	// need to update value's type
+  // need to update value's type
 
-	protected setState(key: keyof S, value: S[keyof S]) {
-		if (!this.state) {
-			return;
-		}
+  protected setState(key: keyof S, value: S[keyof S]) {
+    if (!this.state) {
+      return
+    }
 
-		// if (typeof this.state[key] !== typeof value){
-		// 	console.error(`Cannot change the type of state's ${key}`)
-		// 	return ;
-		// }
-		// // typeof array
-		// // same length -> update
-		// if (value instanceof Array && value.length !== this.state[key].length){
+    // if (typeof this.state[key] !== typeof value){
+    // 	console.error(`Cannot change the type of state's ${key}`)
+    // 	return ;
+    // }
+    // // typeof array
+    // // same length -> update
+    // if (value instanceof Array && value.length !== this.state[key].length){
 
-		// }
+    // }
 
-		// diff length -> just new render
+    // diff length -> just new render
 
-		// element's contain old state -> update
+    // element's contain old state -> update
 
-		// element's attribute diff,
+    // element's attribute diff,
 
-		this.state[key] = value;
-		this.reRender();
-	}
+    this.state[key] = value
+    this.reRender()
+  }
 
-	public getState(key: keyof S) {
-		if (!this.state) {
-			return;
-		}
+  public getState(key: keyof S) {
+    if (!this.state) {
+      return
+    }
 
-		return this.state[key];
-	}
+    return this.state[key]
+  }
 
-	protected init() {
-		this.element = this.render();
-		this.componentDidMount();
-	}
+  protected init() {
+    this.element = this.render()
+    this.componentDidMount()
+  }
 
-	protected abstract render?();
-	protected componentDidMount() {}
-	protected comopnentWillUnmount() {}
-	// private replaceWithNewElement() {
-	// 	const oldElement = this.element;
-	// 	const newElement = this.render();
-	// 	this.element = newElement;
-	// 	oldElement.remove();
-	// }
+  protected abstract render?()
+  protected componentDidMount() {}
+  protected comopnentWillUnmount() {}
+  // private replaceWithNewElement() {
+  // 	const oldElement = this.element;
+  // 	const newElement = this.render();
+  // 	this.element = newElement;
+  // 	oldElement.remove();
+  // }
 
-	// private updateTextValue() {}
+  // private updateTextValue() {}
 }
 
-export default Component;
+export default Component
