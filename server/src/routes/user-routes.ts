9b988ef493d66fc764bc109a175e0f707553f87e
parkjihwanjay <cc6656@naver.com>
2020-07-31 16:10:21 +0900

    github 로그인 테스트 코드

--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -3,8 +3,8 @@ import { validateBody } from "../middlewares/validate-body";
 import { signUpWithId, loginController } from "../service/user-service";
 import { ISignUpBody, ILoginBody } from "../repository/user-repository";
 import passport from "../passport";
-// import { requestAccessToken, accessGithubApi } from "../api/user";
-// import { request } from "http";
+import { requestAccessToken, accessGithubApi } from "../api/user";
+import { request } from "http";
 const userRouter = Router();
 
 userRouter.post(
@@ -20,35 +20,35 @@ userRouter.post(
   loginController
 );
 
-// userRouter.get("/github-login", async (req, res) => {
-//   const baseUrl = "http://localhost:3000";
-//   const queryObject = new URL(baseUrl + req.originalUrl).searchParams;
-//   console.log(queryObject);
-//   const code = queryObject.get("code");
-//   console.log(code);
-//   const clientId = "b11e2ef0ed1a313772ad";
-//   const clientSecret = "2cda7b2a0fb9de981c9923388e76b188cbcd55c3";
+userRouter.get("/github-login", async (req, res) => {
+  const baseUrl = "http://localhost:3000";
+  const queryObject = new URL(baseUrl + req.originalUrl).searchParams;
+  console.log(queryObject);
+  const code = queryObject.get("code");
+  console.log(code);
+  const clientId = "b11e2ef0ed1a313772ad";
+  const clientSecret = "2cda7b2a0fb9de981c9923388e76b188cbcd55c3";
 
-//   const accessTokenUrl = "https://github.com/login/oauth/access_token";
-//   const requestBody = {
-//     client_id: clientId,
-//     client_secret: clientSecret,
-//     code,
-//     redirect_url: "http://localhost:9000",
-//   };
-//   const [response, err] = await requestAccessToken(requestBody);
-//   if (err) {
-//     console.log(err);
-//   }
-//   const accessToken = response.access_token;
+  const accessTokenUrl = "https://github.com/login/oauth/access_token";
+  const requestBody = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    code,
+    redirect_url: "http://localhost:9000",
+  };
+  const [response, err] = await requestAccessToken(requestBody);
+  if (err) {
+    console.log(err);
+  }
+  const accessToken = response.access_token;
 
-//   const [result, error] = await accessGithubApi({ token: accessToken });
-//   if (error) {
-//     console.log(error);
-//   }
-//   console.log(result);
-//   res.redirect("http://localhost:9000");
-// });
+  const [result, error] = await accessGithubApi({ token: accessToken });
+  if (error) {
+    console.log(error);
+  }
+  console.log(result);
+  res.redirect("http://localhost:9000");
+});
 
 // userRouter.post("/social-user", validateBody(["name", "socialId"]), createUser);
 
