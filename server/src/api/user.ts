2b63bb994646e02b92ae9565ae19b3abb47c69aa
parkjihwanjay <cc6656@maver.com>
2020-08-01 20:29:31 +0900

    필요없는 requestAccessToken 함수 삭제

--- server/src/api/user.ts
+++ server/src/api/user.ts
@@ -1,27 +1,8 @@
-import {
-  nodeGetFetchWrapper,
-  nodePopstFetchWrapper,
-} from "../utils/fetch-wrapper";
-
-export interface IrequestAccessToken {
-  client_id: string;
-  client_secret: string;
-  code: string;
-  redirect_url?: string;
-  state?: string;
-}
+import { nodeGetFetchWrapper } from "../utils/fetch-wrapper";
 
 export interface IaccessGithubApi {
   token: string;
 }
-
-export const requestAccessToken = async (args: IrequestAccessToken) => {
-  return await nodePopstFetchWrapper<undefined, IrequestAccessToken>(
-    "POST",
-    "https://github.com/login/oauth/access_token",
-    args
-  );
-};
 
 export const accessGithubApi = async (args: IaccessGithubApi) => {
   return await nodeGetFetchWrapper<undefined, IaccessGithubApi>(
