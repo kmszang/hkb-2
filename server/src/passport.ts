33f157b962e8714ea859d86d411899b635c7d480
parkjihwanjay <cc6656@maver.com>
2020-08-01 20:28:33 +0900

    github 로그인 passport 설정
    * provider 전략 설정
    * 관련된 url 변수화
    * clientId, clientSecret 환경변수화

--- server/src/passport.ts
+++ server/src/passport.ts
@@ -3,6 +3,7 @@ import LocalStrategy from "passport-local";
 import OAuth2Strategy from "passport-oauth2";
 import { User } from "./repository/user-repository";
 import encrypt from "./utils/encryption";
+import { accessGithubApi } from "./api/user";
 passport.use(
   new LocalStrategy({ usernameField: "userId" }, async function (
     username: string,
@@ -24,40 +25,34 @@ passport.use(
   })
 );
 
-const clientId = "b11e2ef0ed1a313772ad";
-const clientSecret = "2cda7b2a0fb9de981c9923388e76b188cbcd55c3";
 const baseUrl = "http://localhost:3000";
-const redirectUrl = `${baseUrl}/api/github-login`;
-const githubLoginuUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_url=${redirectUrl}`;
+const githubLoginUrl = "https://github.com/login/oauth/authorize";
+const githubTokenUrl = "https://github.com/login/oauth/access_token";
+const githubCallbackUrl = `${baseUrl}/api/github-login/callback`;
 
-passport.use(
-  new OAuth2Strategy(
-    {
-      authorizationURL: githubLoginuUrl,
-      tokenURL: "https://github.com/login/oauth/access_token",
-      clientID: clientId,
-      clientSecret: clientSecret,
-      callbackURL: "http://localhost:9000",
-    },
-    async function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ exampleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
-      // const [[user, _], err] = await User.getWithUserId(username);
-      // // console.log(user, err);
-      // if (err) {
-      //   return done(err);
-      // }
-      // if (!user) {
-      //   return done(null, false, { message: "Incorrect username." });
-      // }
-      // if (user.password !== encrypt(password)) {
-      //   return done(null, false, { message: "Incorrect password." });
-      // }
-      // return done(null, user);
-    }
-  )
-);
+const githubOption = {
+  authorizationURL: githubLoginUrl,
+  tokenURL: githubTokenUrl,
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: githubCallbackUrl,
+};
+
+const githubCallback = async (accessToken, refreshToken, profile, done) => {
+  console.log(accessToken, refreshToken, profile);
+  [profile] = await accessGithubApi({ token: accessToken });
+  let [[user, _], err] = await User.getWithSocialId(profile.id);
+  if (!user) {
+    let [[userId, _], err] = await User.createWithSocial({
+      social_id: profile.id,
+      name: profile.login,
+    });
+    [[user, _], err] = await User.getWithId(userId);
+  }
+  return done(err, user);
+};
+
+passport.use("provider", new OAuth2Strategy(githubOption, githubCallback));
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
