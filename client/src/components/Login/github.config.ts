a81d4b7ee62cec17707931622d52f3d740fff301
parkjihwanjay <cc6656@maver.com>
2020-08-02 23:24:31 +0900

    github.config.ts 파일 삭제
    * 해당 내용은 .env로 이동

--- client/src/components/Login/github.config.ts
+++
@@ -1,6 +0,0 @@
-const clientId = 'b11e2ef0ed1a313772ad'
-const baseUrl = 'http://localhost:3000'
-const redirectUrl = `${baseUrl}/api/github-login`
-const githubLoginuUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_url=${redirectUrl}`
-
-export default githubLoginuUrl
