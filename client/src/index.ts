15bb0a030908f420c57feba784b728f92552ec34
andy nam <naamoonoo.y@gmail.com>
2020-08-03 01:59:51 +0900

    [#54] chore: normalize css move to entry point

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,9 @@
-import { App } from './components/App/index'
-import { domRenderer } from './utils/wooact'
+import './styles/_normalize.scss'
 import './styles/_fonts.scss'
 
-domRenderer(new App({}), document.querySelector('#App'))
+import { App } from './components/App'
+import { History } from './pages/History'
+import { domRenderer } from './utils/wooact'
+
+domRenderer(new History(), document.querySelector('#App'))
+// domRenderer(new App({}), document.querySelector('#App'))
