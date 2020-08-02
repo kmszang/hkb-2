dc81518ba5ea58b1aba7901e598a9915525e1c81
andy nam <naamoonoo.y@gmail.com>
2020-08-03 01:59:22 +0900

    [#54] feat: update box input with debouncer

--- client/src/components/BoxInput/BoxInput.ts
+++ client/src/components/BoxInput/BoxInput.ts
@@ -2,32 +2,41 @@ import { Component } from '../../utils/wooact'
 import { div, input, i, p } from '../../utils/wooact/defaultElements'
 
 interface IProps {
-  iconName: string
+  iconName?: string
   name: string
   placeholder: string
-  type: string
+  type?: string
   validateHandler: (target: HTMLInputElement) => boolean
   errMessage: string
 }
+
 interface IState {}
 
 class BoxInput extends Component<IProps, IState, undefined> {
   private errorElement: HTMLElement
+  private debounceTimeout
+
   constructor(props: IProps) {
     super({ props })
+
     Object.setPrototypeOf(this, BoxInput.prototype)
     this.init()
     this.errorElement = this.element.querySelector('p')
   }
 
   onChagneHandler(e: Event) {
-    const $target = e.target as HTMLInputElement
-    const isValidInput = this.props.validateHandler($target)
-    if (isValidInput) {
-      return this.errorElement.classList.remove('visible')
+    if (this.debounceTimeout) {
+      window.clearTimeout(this.debounceTimeout)
     }
-    this.errorElement.classList.add('visible')
-    // $target.focus()
+
+    this.debounceTimeout = window.setTimeout(() => {
+      const $target = e.target as HTMLInputElement
+      const isValidInput = this.props.validateHandler($target)
+      if (isValidInput) {
+        return this.errorElement.classList.remove('visible')
+      }
+      this.errorElement.classList.add('visible')
+    }, 300)
   }
 
   render() {
@@ -35,15 +44,20 @@ class BoxInput extends Component<IProps, IState, undefined> {
 
     return div(
       { className: 'box-input-container' },
-      i({ className: `framework7-${iconName}` }),
-      input({
-        placeholder,
-        name,
-        type,
-        iconName: iconName,
-        oninput: (e) => this.onChagneHandler(e),
-        onfocusout: (e) => this.onChagneHandler(e),
-      }),
+      div(
+        {
+          className: 'inputs',
+        },
+        i({ className: `f7-icons`, textContent: iconName }),
+        input({
+          placeholder,
+          name,
+          type: 'text',
+          iconName: iconName,
+          oninput: (e) => this.onChagneHandler(e),
+          // onfocusout: (e) => this.onChagneHandler(e),
+        })
+      ),
       p({ textContent: errMessage, className: 'error' })
     )
   }
