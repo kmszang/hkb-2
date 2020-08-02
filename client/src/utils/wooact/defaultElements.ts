6664e3869f3268662bf04b8e39434efda6253e44
andy nam <naamoonoo.y@gmail.com>
2020-08-02 21:44:56 +0900

    [#38] feat: wooact diff algorithm update

--- client/src/utils/wooact/defaultElements.ts
+++ client/src/utils/wooact/defaultElements.ts
@@ -1,4 +1,4 @@
-import { IAttribute, createElement, CreateElement } from './createElement'
+import { IAttribute, createElement } from './createElement'
 import { Component } from './'
 
 const htmlTagNames = [
@@ -124,8 +124,6 @@ const htmlTagNames = [
   'wbr',
 ] as const
 
-type HTMLDefaultTag = typeof htmlTagNames[number]
-
 export const [
   a,
   abbr,
@@ -250,156 +248,6 @@ export const [
 ] = htmlTagNames.map(
   (tagName) => (
     attributes: IAttribute,
-    ...childNodes: (HTMLElement | Component<any, any>)[]
+    ...childNodes: (HTMLElement | Component<any, any, any>)[]
   ) => createElement(tagName, attributes, ...childNodes)
 )
-
-export type HTMLELementTagName = keyof Omit<
-  HTMLElementTagNameMap,
-  'var' | 'object' | 'acronym'
->
-type HTMLElementTagType = HTMLElementTagNameMap[HTMLELementTagName]
-
-// in progress
-// export const D: {} = htmlTagNames.reduce<
-// 	{
-// 		[K in keyof HTMLElementTagNameMap]: CreateElement<
-// 			HTMLElementTagNameMap[K]
-// 		>;
-// 	}
-// >((acc, tagName) => {
-// 	acc[tagName] = (
-// 		attributes: IAttribute,
-// 		...childNodes: (HTMLElement | Component<any, any>)[]
-// 	) => createElement(tagName, attributes, ...childNodes);
-// 	return acc;
-// }, {});
-
-// export default htmlTagNames.reduce(
-//   (acc, tagName) => (
-//     attributes: IAttribute,
-//     ...childNodes: HTMLElement[] | Component<any, any>[]
-//   ) => {
-//     acc[tagName] = createElement(tagName, attributes, ...childNodes)
-//     return acc
-//   },
-//   {}
-// )
-
-// const HtmlTagAndTypes: {} = {
-//   aside: HTMLElement,
-//   button: HTMLButtonElement,
-//   div: HTMLDivElement,
-//   footer: HTMLElement,
-//   form: HTMLFormElement,
-//   h1: HTMLHeadingElement,
-//   h2: HTMLHeadingElement,
-//   h3: HTMLHeadingElement,
-//   h4: HTMLHeadingElement,
-//   h5: HTMLHeadingElement,
-//   h6: HTMLHeadingElement,
-//   header: HTMLElement,
-//   input: HTMLInputElement,
-//   label: HTMLLabelElement,
-//   nav: HTMLElement,
-//   p: HTMLParagraphElement,
-//   q: HTMLQuoteElement,
-// }
-
-// const TagNames = Object.keys(HtmlTagAndTypes)
-// export const [
-//   aside,
-//   button,
-//   div,
-//   footer,
-//   form,
-//   h1,
-//   h2,
-//   h3,
-//   h4,
-//   h5,
-//   h6,
-//   header,
-//   input,
-//   label,
-//   nav,
-//   p,
-//   q,
-// ] = TagNames.map((htmlTagName) => {
-//   const type = HtmlTagAndTypes[htmlTagName]
-//   return (tagName: string, type: Type) => (
-//     attributes: IAttribute,
-//     ...childNodes: HTMLElement[] | Component<any, any>[]
-//   ) => createElement<type>(tagName, attributes, ...childNodes)
-// })
-
-// export const div = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('div', attributes, ...childNodes)
-
-// export const a = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('a', attributes, ...childNodes)
-
-// export const p = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('p', attributes, ...childNodes)
-
-// export const span = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('span', attributes, ...childNodes)
-
-// export const button = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('button', attributes, ...childNodes)
-
-// export const input = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('input', attributes, ...childNodes)
-
-// export const form = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('form', attributes, ...childNodes)
-
-// export const label = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('label', attributes, ...childNodes)
-
-// export const q = (
-//   attributes: IAttribute,
-//   ...childNodes: HTMLElement[] | Component<any, any>[]
-// ) => createElement('q', attributes, ...childNodes)
-
-// export const [h1, h2, h3, h4, h5, h6] = [
-//   'h1',
-//   'h2',
-//   'h3',
-//   'h4',
-//   'h5',
-//   'h6',
-// ].map(
-//   (tagName) => (
-//     attributes: IAttribute,
-//     ...childNodes: HTMLElement[] | Component<any, any>[]
-//   ) => createElement(tagName, attributes, ...childNodes)
-// )
-// export const [aside, footer, header, nav, main] = [
-//   'aside',
-//   'footer',
-//   'header',
-//   'nav',
-//   'main',
-// ].map(
-//   (tagName) => (
-//     attributes: IAttribute,
-//     ...childNodes: HTMLElement[] | Component<any, any>[]
-//   ) => createElement(tagName, attributes, ...childNodes)
-// )
