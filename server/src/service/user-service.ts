7bb83048ac0c927ef1946235b9f47b93b0e5637b
andy nam <naamoonoo.y@gmail.com>
2020-08-02 23:32:19 +0900

    [#52] chore: fix merge conflict

--- server/src/service/user-service.ts
+++ server/src/service/user-service.ts
@@ -1,26 +1,34 @@
 import { Request, Response } from "express";
 import { User, ISignUpBody } from "../repository/user-repository";
-import encrypt from "../utils/encryption";
+import { encrypt } from "../utils/encryption";
+
+declare global {
+	namespace Express {
+		interface Request {
+			user: any;
+		}
+	}
+}
 
 export const signUpWithId = async (req: Request, res: Response) => {
-  const { userId, password, name } = req.body as ISignUpBody;
-  const encryptedPassword = encrypt(password);
-  const [insertedUser, error] = await User.createWithId({
-    userId,
-    name,
-    password: encryptedPassword,
-  });
-  if (error) {
-    throw new Error(error);
-  }
+	const { userId, password, name } = req.body as ISignUpBody;
+	const encryptedPassword = encrypt(password);
+	const [insertedUser, error] = await User.createWithId({
+		userId,
+		name,
+		password: encryptedPassword,
+	});
+	if (error) {
+		throw new Error(error);
+	}
 
-  res.json(insertedUser);
+	res.json(insertedUser);
 };
 
 export const loginController = async (req: Request, res: Response) => {
-  //   console.log(req.session.cookie);
-  if (!req.user) {
-    return res.json("로그인을 다시 해주세요");
-  }
-  return res.json("welcome");
+	//   console.log(req.session.cookie);
+	if (!req.user) {
+		return res.json("로그인을 다시 해주세요");
+	}
+	return res.json("welcome");
 };
