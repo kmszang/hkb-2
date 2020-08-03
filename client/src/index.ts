6d29b8ac416a3f51865404f1b60be2e4e20c32ab
andy nam <naamoonoo.y@gmail.com>
2020-08-03 17:37:27 +0900

    [#56] feat: router setup with routing utils

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,8 +2,6 @@ import './styles/_normalize.scss'
 import './styles/_fonts.scss'
 
 import { App } from './components/App'
-import { History } from './pages/History'
 import { domRenderer } from './utils/wooact'
 
-domRenderer(new History(), document.querySelector('#App'))
-// domRenderer(new App({}), document.querySelector('#App'))
+domRenderer(new App({}), document.querySelector('#App'))
