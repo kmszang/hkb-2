ba40b5232dd14114185b621508a54e03a9a7d3be
andy nam <naamoonoo.y@gmail.com>
2020-08-04 15:58:15 +0900

    [#61] chore: remove useless comments

--- client/src/modules/index.ts
+++ client/src/modules/index.ts
@@ -1,11 +1,9 @@
 import { Store } from '../utils/wooact/Store'
-import { Component } from '../utils/wooact'
 import { TransactionStore } from './TransactionStore'
 import { IMonth, MonthStore } from './MonthStore'
 import { ITransactionResponse } from '../api/transaction'
 
 export interface ICombinedStore {
-  // [storeName: string]: Store<any>
   transaction: Store<ITransactionResponse[]>
   month: Store<IMonth>
 }
@@ -17,11 +15,3 @@ export const combinedStore: ICombinedStore = {
   transaction: transactionStore,
   month: monthStore,
 }
-
-// export const connectStore = (
-//   storeName: keyof ICombinedStore,
-//   component: Component<any, any>
-// ) => {
-//   combinedStore[storeName].subscribe(component)
-//   return combinedStore[storeName]
-// }
