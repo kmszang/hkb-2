82bfb14153b29775997193946696f04d1c48ca9c
andy nam <naamoonoo.y@gmail.com>
2020-08-06 17:52:10 +0900

    [#76] feat: add initialize dateStore

--- client/src/modules/index.ts
+++ client/src/modules/index.ts
@@ -2,6 +2,7 @@ import { Store } from '../utils/Store'
 import { TransactionStore } from './TransactionStore'
 import { IVisible, VisibleStore } from './visibleStore'
 import { CategoryStore } from './CategoryStore'
+import { DateInfo, DateStore } from './DateStore'
 import { ITransactionResponse } from '../api/transaction'
 import { ICategoryResponse } from '../api/category'
 
@@ -9,21 +10,23 @@ export interface ICombinedStore {
   transaction: Store<ITransactionResponse[]>
   visible: Store<IVisible>
   category: Store<ICategoryResponse[]>
+  date: Store<DateInfo>
+}
+
+const today = new Date()
+const initialDate = {
+  month: today.getMonth() + 1,
+  year: today.getFullYear(),
 }
 
 const transactionStore = new TransactionStore()
 const visibleStore = new VisibleStore({ income: true, outcome: true })
 const categoryStore = new CategoryStore()
+const dateStore = new DateStore(initialDate)
 
 export const combinedStore: ICombinedStore = {
   transaction: transactionStore,
   visible: visibleStore,
   category: categoryStore,
+  date: dateStore,
 }
-
-// export const combinedStoreData = {
-//   transaction: { updated: false, data: transactionStore.data },
-//   month: { updated: false, data: monthStore.data },
-// }
-
-// fireEvent(STORE_UPDATED, { month: initialMonth })
