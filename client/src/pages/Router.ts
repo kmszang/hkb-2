a900ce012c97f74a3421b12ce880dee204b72dae
andy nam <naamoonoo.y@gmail.com>
2020-08-06 17:50:11 +0900

    [#76] feat: router component added

---
+++ client/src/pages/Router.ts
@@ -0,0 +1,50 @@
+import { Component } from '../utils/wooact'
+import { Transaction } from './Transaction'
+import { SignIn } from './SignIn'
+import { Statistics } from './Statistics'
+import { Calendar } from './Calendar'
+import { Routing } from '../utils/Routing'
+import { div } from '../utils/wooact/defaultElements'
+import { Header } from '../components/Header'
+
+export const SIGN_IN = '/sign-in' as const
+export const TRANSACTION = '/' as const
+export const STATISTICS = '/statistics' as const
+export const CALENDAR = '/calendar' as const
+
+interface IProps {}
+interface IState {}
+
+export class RouterComponent extends Component<IProps, IState> {
+  public routing
+
+  constructor() {
+    super()
+
+    Object.setPrototypeOf(this, RouterComponent.prototype)
+    this.routing = new Routing({
+      [TRANSACTION]: Transaction,
+      [SIGN_IN]: SignIn,
+      [STATISTICS]: Statistics,
+      [CALENDAR]: Calendar,
+    })
+    this.routing.init(this)
+    this.init()
+  }
+
+  getHeaderTitle(rawTitle: string) {
+    const title = rawTitle.slice(1)
+    if (!title) {
+      return 'Transaction'
+    }
+    return title.toUpperCase()[0] + title.slice(1)
+  }
+
+  render() {
+    return div(
+      { className: 'main-container' },
+      new Header({ title: this.getHeaderTitle(this.routing.getPath()) }),
+      this.routing.getPage()
+    )
+  }
+}
