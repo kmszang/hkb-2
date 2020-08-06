2c76e1226562bb22e97d9fcccd5f33053d61b6fb
andy nam <naamoonoo.y@gmail.com>
2020-08-07 02:25:59 +0900

    [#63] feat: update data type

--- client/src/api/category.ts
+++ client/src/api/category.ts
@@ -1,14 +1,6 @@
 import { fetchWrapper } from '../utils/fetchWrapper'
 import { CATEGORY } from './apiRoutes'
 
-export interface ICreateTransaction {
-  content: string
-  price: number
-  paymentId: number
-  userId: number
-  categoryId: number
-}
-
 export interface ICategoryResponse {
   id: number
   name: string
