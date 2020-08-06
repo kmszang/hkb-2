a8e514e3624f9ad0e27be6c59f517a74495b1bf7
andy nam <naamoonoo.y@gmail.com>
2020-08-06 17:49:09 +0900

    [#76] feat: split router from app

--- client/src/components/App/App.ts
+++ client/src/components/App/App.ts
@@ -1,7 +1,6 @@
 import { Component } from '../../utils/wooact'
 import { div } from '../../utils/wooact/defaultElements'
-import { Header } from '../Header/index'
-import { routing } from '../../utils/Routing'
+import { RouterComponent } from '../../pages/Router'
 import { FETCH_ALL_TRANSACTION } from '../../modules/TransactionStore'
 import { FETCH_ALL_CATEGORIES } from '../../modules/CategoryStore'
 import { SideBar } from '../SideBar'
@@ -15,38 +14,26 @@ class App extends Component<IProps, IState> {
 
     Object.setPrototypeOf(this, App.prototype)
     this.connectAction('transaction', 'category')
-    routing.init(this)
     this.init()
   }
 
   async componentDidMount() {
     const today = new Date()
-    this.store.transaction.dispatch(FETCH_ALL_TRANSACTION, {
+    const initialDate = {
       month: today.getMonth() + 1,
       year: today.getFullYear(),
+    }
+
+    this.store.transaction.dispatch(FETCH_ALL_TRANSACTION, {
+      ...initialDate,
     })
     this.store.category.dispatch(FETCH_ALL_CATEGORIES)
   }
 
-  getHeaderTitle(rawTitle: string) {
-    const title = rawTitle.slice(1)
-    if (!title) {
-      return 'Transaction'
-    }
-    return title.toUpperCase()[0] + title.slice(1)
-  }
-
   render() {
-    return div(
-      {},
-      new SideBar(),
+    const router = new RouterComponent()
 
-      div(
-        { className: 'main-container' },
-        new Header({ title: this.getHeaderTitle(routing.getPath()) }),
-        routing.getPage()
-      )
-    )
+    return div({}, new SideBar({ routing: router.routing }), router)
   }
 }
 
