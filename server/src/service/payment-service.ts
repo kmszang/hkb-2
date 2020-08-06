4805af0513da2ffffec8fa41d5a7a2b110ae9b7b
andy nam <naamoonoo.y@gmail.com>
2020-08-06 19:19:40 +0900

    [#58] feat: implement payment service

---
+++ server/src/service/payment-service.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import { DatabaseError } from "../errors/database-error";
+import { Payment, IUserPayment } from "../repository/payment-repository";
+
+export const getAllPayments = async (req: Request, res: Response) => {
+	const [AllPayments, fetchError] = await Payment.getAll();
+
+	if (fetchError) {
+		throw new DatabaseError(fetchError);
+	}
+
+	res.json(AllPayments);
+};
+
+export const getUsersPayments = async (req: Request, res: Response) => {
+	const { userId } = req.params;
+	const [userPayment, fetchError] = await Payment.getUserSelectedPayment(
+		+userId
+	);
+
+	if (fetchError) {
+		throw new DatabaseError(fetchError);
+	}
+
+	res.json(userPayment);
+};
+
+export const addNewPayment = async (req: Request, res: Response) => {
+	const args = req.body as IUserPayment;
+	const [existedPayment, fetchError] = await Payment.getOne(args);
+
+	if (existedPayment.length !== 0) {
+		res.json(args.paymentId);
+		return;
+	}
+
+	const [insertedtransactionId, insertError] = await Payment.addPayment(args);
+
+	if (insertError) {
+		throw new DatabaseError(insertError);
+	}
+
+	res.json(args.paymentId);
+};
+
+export const deletePayment = async (req: Request, res: Response) => {
+	const args = req.body as IUserPayment;
+	const [removedRows, removeError] = await Payment.deletePayment(args);
+
+	if (removeError || removedRows !== 1) {
+		throw new DatabaseError(removeError);
+	}
+
+	res.json(args.paymentId);
+};
