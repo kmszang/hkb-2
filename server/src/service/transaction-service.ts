9aabab8ee139386c75946267d2768ab02177fb09
andy nam <naamoonoo.y@gmail.com>
2020-08-06 14:53:31 +0900

    [#72] feat: fix getAll, add update, delete service

--- server/src/service/transaction-service.ts
+++ server/src/service/transaction-service.ts
@@ -3,6 +3,7 @@ import { DatabaseError } from "../errors/database-error";
 import {
 	Transaction,
 	ICreateTransaction,
+	IUpdateTransaction,
 } from "../repository/transaction-repository";
 
 export const createNewTransaction = async (req: Request, res: Response) => {
@@ -25,11 +26,42 @@ export const createNewTransaction = async (req: Request, res: Response) => {
 };
 
 export const getAllTransactions = async (req: Request, res: Response) => {
-	const [allTransactions, fetchError] = await Transaction.getAll();
+	const { date } = req.params;
+	const [allTransactions, fetchError] = await Transaction.getAll(date);
 
 	if (fetchError) {
 		throw new DatabaseError(fetchError);
 	}
 
 	res.json(allTransactions);
+};
+
+export const updateTransaction = async (req: Request, res: Response) => {
+	const args = req.body as IUpdateTransaction;
+	const [updatedRows, updateError] = await Transaction.update(req.body);
+
+	if (updateError || updatedRows !== 1) {
+		throw new DatabaseError(updateError);
+	}
+
+	const [[updatedTransaction, _], fetchError] = await Transaction.getOne(
+		args.id
+	);
+
+	if (fetchError) {
+		throw new DatabaseError(fetchError);
+	}
+
+	res.json(updatedTransaction);
+};
+
+export const deleteTransaction = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const [removedRows, removeError] = await Transaction.delete(parseInt(id));
+
+	if (removeError || removedRows !== 1) {
+		throw new DatabaseError(removeError);
+	}
+
+	res.json("");
 };
