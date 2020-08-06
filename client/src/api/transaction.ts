e9880c825c1337b13da96e4f8c238a09b6bf113a
andy nam <naamoonoo.y@gmail.com>
2020-08-06 15:04:10 +0900

    [#78] feat: add delete, update api  fetcher from client

--- client/src/api/transaction.ts
+++ client/src/api/transaction.ts
@@ -1,5 +1,6 @@
 import { fetchWrapper } from '../utils/fetchWrapper'
 import { TRANSACTION } from './apiRoutes'
+import { addTimeDiff } from '../utils/timeAdjust'
 
 export interface ICreateTransaction {
   content: string
@@ -7,6 +8,10 @@ export interface ICreateTransaction {
   paymentId: number
   userId: number
   categoryId: number
+}
+
+export interface IUpdateTransaction extends Partial<ICreateTransaction> {
+  id: number
 }
 
 export interface ITransactionResponse {
@@ -20,10 +25,11 @@ export interface ITransactionResponse {
   iconName: string
 }
 
-export const fetchAllTransaction = async () => {
+export const fetchAllTransaction = async (year: number, month: number) => {
+  const date = `${year}-${month}`
   return await fetchWrapper<ITransactionResponse[], undefined>(
     'GET',
-    TRANSACTION
+    TRANSACTION + `/${date}`
   )
 }
 
@@ -32,5 +38,20 @@ export const createNewTransaction = async (args: ICreateTransaction) => {
     'POST',
     TRANSACTION,
     args
+  )
+}
+
+export const updateTransaction = async (args: IUpdateTransaction) => {
+  return await fetchWrapper<ITransactionResponse, IUpdateTransaction>(
+    'PUT',
+    TRANSACTION,
+    args
+  )
+}
+
+export const deleteTransaction = async (id: number) => {
+  return await fetchWrapper<ITransactionResponse, IUpdateTransaction>(
+    'PUT',
+    TRANSACTION + `/${id}`
   )
 }
