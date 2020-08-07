31f64fd52cae972344c87be9136dcc2178fd9309
andy nam <naamoonoo.y@gmail.com>
2020-08-07 13:57:55 +0900

    .

--- server/src/repository/payment-repository.ts
+++ server/src/repository/payment-repository.ts
@@ -1,35 +1,35 @@
 // SELECT P.name FROM Payment as P JOIN User_Payment_Method as R ON R.payment_id=P.id JOIN User as U ON U.id = R.user_id Where R.is_active=true;
 import {
-  insertQueryExecuter,
-  selectQueryExecuter,
-  updateOrDeleteQueryExecuter,
+	insertQueryExecuter,
+	selectQueryExecuter,
+	updateOrDeleteQueryExecuter,
 } from "../utils/query-executor";
 
 export interface IPayment {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
 export interface IUserPayment {
-  userId?: number;
-  paymentId: number;
+	userId?: number;
+	paymentId: number;
 }
 
 export class Payment {
-  static async getAll() {
-    const selectAllPayment = `
+	static async getAll() {
+		const selectAllPayment = `
 			SELECT
 				*
 			FROM
 				Payment;
 			`;
-    return await selectQueryExecuter<IPayment>(selectAllPayment);
-  }
+		return await selectQueryExecuter<IPayment>(selectAllPayment);
+	}
 
-  static async getOne(args: IUserPayment) {
-    const { userId, paymentId } = args;
+	static async getOne(args: IUserPayment) {
+		const { userId, paymentId } = args;
 
-    const selectUsersPayment = `
+		const selectUsersPayment = `
 			SELECT
 				P.id, P.name
 			FROM
@@ -39,13 +39,13 @@ export class Payment {
 			JOIN
 				User as U ON U.id = R.user_id
 			WHERE
-				U.id=${userId} AND P.id=${paymentId} AND R.is_active=true
+				U.id=${78} AND P.id=${paymentId} AND R.is_active=true
 			;`;
-    return await selectQueryExecuter<IPayment>(selectUsersPayment);
-  }
+		return await selectQueryExecuter<IPayment>(selectUsersPayment);
+	}
 
-  static async getUserSelectedPayment(userId: number) {
-    const selectUsersPayment = `
+	static async getUserSelectedPayment(userId: number) {
+		const selectUsersPayment = `
 			SELECT
 				P.id, P.name
 			FROM
@@ -55,32 +55,32 @@ export class Payment {
 			JOIN
 				User as U ON U.id = R.user_id
 			WHERE
-				U.id=${userId} AND R.is_active=true
+				U.id=${78} AND R.is_active=true
 			;`;
-    return await selectQueryExecuter<IPayment>(selectUsersPayment);
-  }
+		return await selectQueryExecuter<IPayment>(selectUsersPayment);
+	}
 
-  static async addPayment(args: IUserPayment) {
-    const { userId, paymentId } = args;
-    const insertUsersPayment = `
+	static async addPayment(args: IUserPayment) {
+		const { userId, paymentId } = args;
+		const insertUsersPayment = `
 			INSERT INTO
 				User_Payment_Method(payment_id, user_id)
 			VALUES
-				("${paymentId}", "${userId}")
+				("${paymentId}", "${78}")
 			;`;
-    return await insertQueryExecuter(insertUsersPayment);
-  }
+		return await insertQueryExecuter(insertUsersPayment);
+	}
 
-  static async deletePayment(args: IUserPayment) {
-    const { userId, paymentId } = args;
-    const deleteUserPayment = `
+	static async deletePayment(args: IUserPayment) {
+		const { userId, paymentId } = args;
+		const deleteUserPayment = `
 			UPDATE
 				User_Payment_Method
 			SET
 				is_active=false
 		 	WHERE
-				user_id=${userId} AND payment_id=${paymentId}
+				user_id=${78} AND payment_id=${paymentId}
 		 	;`;
-    return await updateOrDeleteQueryExecuter(deleteUserPayment);
-  }
+		return await updateOrDeleteQueryExecuter(deleteUserPayment);
+	}
 }
