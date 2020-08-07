9ec15216976e424d4a9eab84672f661c4878a739
andy nam <naamoonoo.y@gmail.com>
2020-08-07 10:45:05 +0900

    [#63] feat: transaction item can delete and update

--- client/src/components/TransactionItem/TransactionItem.ts
+++ client/src/components/TransactionItem/TransactionItem.ts
@@ -1,35 +1,51 @@
 import { Component } from '../../utils/wooact'
-import { div, p, i } from '../../utils/wooact/defaultElements'
+import { div } from '../../utils/wooact/defaultElements'
 import { ITransactionResponse } from '../../api/transaction'
 import { getCSVNumber } from '../../utils/getCSVNumber'
 import { ICon } from '../ICon'
 import { DELETE_TRANSACTION } from '../../stores/TransactionStore'
+import { AddNewTransaction } from '../AddNewTransaction'
 
 interface IProps {
   transaction: ITransactionResponse
 }
-interface IState {}
+interface IState {
+  isEditMode: boolean
+}
 
 class TransactionItem extends Component<IProps, IState> {
   constructor(props: IProps) {
-    super(props)
+    const state: IState = {
+      isEditMode: false,
+    }
+    super(props, state)
 
     Object.setPrototypeOf(this, TransactionItem.prototype)
     this.connectAction('transaction')
     this.init()
   }
 
+  toggleMode() {
+    this.setState('isEditMode', !this.getState('isEditMode'))
+  }
   renderHandler() {
-    const { dispatch } = this.store.transaction
     const { id } = this.props.transaction
 
     return [
+      new ICon({
+        isSelected: false,
+        onClickHandler: () => this.toggleMode(),
+        iconName: 'wrench',
+        name: '수정',
+        isIncome: true,
+      }),
       new ICon({
         isSelected: false,
         onClickHandler: async () =>
           await this.store.transaction.dispatch(DELETE_TRANSACTION, id),
         iconName: 'trash',
         name: '삭제',
+        isIncome: false,
       }),
     ]
   }
@@ -45,38 +61,51 @@ class TransactionItem extends Component<IProps, IState> {
       isIncome,
     } = this.props.transaction
 
-    return div(
-      {
-        className: 'item-container',
-        id: `item-${id}`,
-        accessKey: 'transaction-item',
-      },
-      new ICon({
-        isSelected: true,
-        isIncome,
-        iconName,
-        name: categoryName,
-        onClickHandler: () => {},
-      }),
-      div(
-        { className: 'info-container' },
-        div({
-          className: 'info-payment',
-          textContent: paymentName,
-        }),
-        div({ className: 'info-content', textContent: content })
-      ),
-      div({
-        className: `price ${isIncome ? 'income' : 'outcome'}`,
-        textContent: getCSVNumber(price),
-      }),
-      div(
-        {
-          className: 'item-handler-container',
-        },
-        ...this.renderHandler()
-      )
-    )
+    const isEditMode = this.getState('isEditMode')
+    const updateInputs = new AddNewTransaction({
+      updateTranscation: this.props.transaction,
+      onToggleHandler: () => this.toggleMode(),
+    })
+
+    return isEditMode
+      ? updateInputs.element
+      : div(
+          {
+            className: 'item-container',
+            id: `item-${id}`,
+            accessKey: 'transaction-item',
+          },
+          div(
+            {
+              className: 'overlap',
+            },
+            div(
+              {
+                className: 'item-handler-container',
+              },
+              ...this.renderHandler()
+            )
+          ),
+          new ICon({
+            isSelected: true,
+            isIncome,
+            iconName,
+            name: categoryName,
+            onClickHandler: () => {},
+          }),
+          div(
+            { className: 'info-container' },
+            div({
+              className: 'info-payment',
+              textContent: paymentName,
+            }),
+            div({ className: 'info-content', textContent: content })
+          ),
+          div({
+            className: `price ${isIncome ? 'income' : 'outcome'}`,
+            textContent: getCSVNumber(price),
+          })
+        )
   }
 }
 
