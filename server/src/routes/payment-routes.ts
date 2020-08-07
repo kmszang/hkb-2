e26a8a2dd46102e278b7c5fc8ffd76f47e64a5b5
parkjihwanjay <cc6656@maver.com>
2020-08-07 11:18:31 +0900

    Payment 페이지 router에 추가

--- server/src/routes/payment-routes.ts
+++ server/src/routes/payment-routes.ts
@@ -1,25 +1,25 @@
 import { Router } from "express";
 import { validateBody } from "../middlewares/validate-body";
 import {
-	getAllPayments,
-	getUsersPayments,
-	addNewPayment,
-	deletePayment,
+  getAllPayments,
+  getUsersPayments,
+  addNewPayment,
+  deletePayment,
 } from "../service/payment-service";
 import { IUserPayment } from "../repository/payment-repository";
 
 const paymentRouter = Router();
 
 paymentRouter.post(
-	"/",
-	validateBody<IUserPayment>(["paymentId", "userId"]),
-	addNewPayment
+  "/",
+  validateBody<IUserPayment>(["paymentId", "userId"]),
+  addNewPayment
 );
 
 paymentRouter.delete(
-	"/",
-	validateBody<IUserPayment>(["paymentId", "userId"]),
-	deletePayment
+  "/",
+  validateBody<IUserPayment>(["paymentId", "userId"]),
+  deletePayment
 );
 
 paymentRouter.get("/", getAllPayments);
