8188e1961cd8ea58734512950581e83f897e5e92
andy nam <naamoonoo.y@gmail.com>
2020-08-07 10:44:39 +0900

    [#63] feat: make reusable for update

--- client/src/components/AddNewTransaction/AddNewTransaction.ts
+++ client/src/components/AddNewTransaction/AddNewTransaction.ts
@@ -1,12 +1,24 @@
 import { Component } from '../../utils/wooact'
-import { div, input, button } from '../../utils/wooact/defaultElements'
-import { ICreateTransaction } from '../../api/category'
+import {
+  div,
+  input,
+  button,
+  select,
+  option,
+} from '../../utils/wooact/defaultElements'
+import { ICreateTransaction, ITransactionResponse } from '../../api/transaction'
 import { BoxInput } from '../BoxInput'
-import { ADD_ONE_TRANSACTION } from '../../stores/TransactionStore'
+import {
+  ADD_ONE_TRANSACTION,
+  UPDATE_TRANSACTION,
+} from '../../stores/TransactionStore'
 import { ICon } from '../ICon'
-import { getDate, getFullDateIn } from '../../utils/dateInfos'
+import { dateForMiniCalendar, getFullDateIn } from '../../utils/dateInfos'
 
-interface IProps {}
+interface IProps {
+  updateTranscation: ITransactionResponse
+  onToggleHandler: () => void
+}
 
 interface IState {
   isIncomeMode: number
@@ -22,27 +34,41 @@ interface IMode {
 }
 
 class AddNewTransaction extends Component<IProps, IState> {
-  constructor() {
-    const state: IState = {
-      selectedCategoryId: -1,
-      isIncomeMode: 0,
-      isAddMode: false,
-      date: '',
+  constructor(props?: IProps) {
+    let state: IState
+    if (!props) {
+      state = {
+        selectedCategoryId: -1,
+        isIncomeMode: 0,
+        isAddMode: false,
+        date: '',
+      }
+    } else {
+      const { categoryId, isIncome, createdAt } = props.updateTranscation
+      state = {
+        selectedCategoryId: categoryId,
+        isIncomeMode: isIncome ? 1 : 0,
+        isAddMode: true,
+        date: createdAt.toString(),
+      }
     }
-    super({}, state)
+    super(props, state)
 
     Object.setPrototypeOf(this, AddNewTransaction.prototype)
-    this.connectStore('date', 'transaction', 'category')
+    this.connectStore('date', 'transaction', 'category', 'payment')
     this.init()
   }
 
   renderInputs() {
+    const isUpdatedMode = this.props ? true : false
+
     const contentInput = new BoxInput({
       iconName: 'doc_text',
       name: 'content',
       placeholder: '내용을 입력해주세요.',
       validateHandler: (target: HTMLInputElement) => target.value.length > 0,
       errMessage: '내용이 비어있습니다.',
+      initialValue: isUpdatedMode ? this.props.updateTranscation.content : '',
     })
 
     const priceInput = new BoxInput({
@@ -52,6 +78,7 @@ class AddNewTransaction extends Component<IProps, IState> {
       placeholder: '금액을 입력해주세요.',
       validateHandler: (target: HTMLInputElement) => target.value.length > 0,
       errMessage: '올바른 금액을 입력해주세요.',
+      initialValue: isUpdatedMode ? this.props.updateTranscation.price : '',
     })
 
     return div(
@@ -59,7 +86,8 @@ class AddNewTransaction extends Component<IProps, IState> {
         className: 'inputs-container',
       },
       contentInput,
-      priceInput
+      priceInput,
+      this.renderPayment()
     )
   }
 
@@ -100,7 +128,6 @@ class AddNewTransaction extends Component<IProps, IState> {
     const categories = this.store.category.data
     const isIncomeMode = this.getState('isIncomeMode') as number
     const selectedCategoryId = this.getState('selectedCategoryId') as number
-
     if (!categories || categories.length === 0) {
       return null
     }
@@ -126,41 +153,6 @@ class AddNewTransaction extends Component<IProps, IState> {
     )
   }
 
-  async onAddHandler() {
-    const [contentEle, priceEle] = Array.from(
-      this.element.querySelectorAll('input')
-    ) as HTMLInputElement[]
-
-    const createTransactionBody: ICreateTransaction = {
-      content: contentEle.value,
-      price: parseInt(priceEle.value),
-      paymentId: 4,
-      userId: 78,
-      categoryId: this.getState('selectedCategoryId') as number,
-      date: this.getState('date') as string,
-    }
-
-    for (const key in createTransactionBody) {
-      const value = createTransactionBody[key]
-      if (typeof value === 'number' && value < 0) {
-        return
-      }
-
-      if (typeof value === 'string' && value.length === 0) {
-        return
-      }
-    }
-
-    await this.store.transaction.dispatch(
-      ADD_ONE_TRANSACTION,
-      createTransactionBody
-    )
-
-    contentEle.value = ''
-    priceEle.value = ''
-    this.toggleVisibilty()
-  }
-
   getFullDate() {
     const NUMBER_OF_GRID = 42
     const { year, month } = this.store.date.data
@@ -182,6 +174,7 @@ class AddNewTransaction extends Component<IProps, IState> {
   renderMiniCalendar() {
     const dateSet = this.getFullDate()
     const { year, month } = this.store.date.data
+    const formattedDate = dateForMiniCalendar(this.getState('date') as string)
 
     return div(
       { className: 'calendar-input-container' },
@@ -190,7 +183,7 @@ class AddNewTransaction extends Component<IProps, IState> {
         { className: 'date-table' },
         ...dateSet.map(({ dayName, year, month, date }) => {
           const currentDate = `${year}-${month}-${date}`
-          const isSelected = this.getState('date') === currentDate
+          const isSelected = formattedDate === currentDate
           return div(
             {
               className: `calendar-item-container ${
@@ -208,6 +201,23 @@ class AddNewTransaction extends Component<IProps, IState> {
     )
   }
 
+  renderPayment() {
+    const payments = this.store.payment
+
+    if (!payments.data || payments.data.length === 0) {
+      return null
+    }
+
+    return select(
+      { className: 'payments-input' },
+      ...payments.data
+        .filter(({ selected }) => selected)
+        .map(({ id, name }) =>
+          option({ className: 'option', value: id, textContent: name })
+        )
+    )
+  }
+
   toggleVisibilty() {
     const isAddMode = this.getState('isAddMode')
     this.setState('isIncomeMode', 0)
@@ -216,15 +226,64 @@ class AddNewTransaction extends Component<IProps, IState> {
     this.setState('isAddMode', !isAddMode)
   }
 
+  async onAddHandler() {
+    const payment = this.element.querySelector('select')
+    const [contentEle, priceEle] = Array.from(
+      this.element.querySelectorAll('input')
+    ) as HTMLInputElement[]
+
+    const createTransactionBody: ICreateTransaction = {
+      content: contentEle.value,
+      price: parseInt(priceEle.value),
+      paymentId: +payment.value,
+      userId: 78,
+      categoryId: this.getState('selectedCategoryId') as number,
+      date: this.getState('date') as string,
+    }
+
+    for (const key in createTransactionBody) {
+      const value = createTransactionBody[key]
+      if (typeof value === 'number' && value < 0) {
+        return
+      }
+
+      if (typeof value === 'string' && value.length === 0) {
+        return
+      }
+    }
+
+    if (this.props) {
+      await this.store.transaction.dispatch(UPDATE_TRANSACTION, {
+        ...createTransactionBody,
+        id: this.props.updateTranscation.id,
+      })
+    } else {
+      await this.store.transaction.dispatch(
+        ADD_ONE_TRANSACTION,
+        createTransactionBody
+      )
+    }
+
+    contentEle.value = ''
+    priceEle.value = ''
+    if (this.props) {
+      this.props.onToggleHandler()
+      return
+    }
+    this.toggleVisibilty()
+  }
+
   render() {
     const isAddMode = this.getState('isAddMode')
+    const isUpdatedMode = this.props ? true : false
 
     return div(
       {},
       button({
         className: `toogle-add-input ${isAddMode ? 'close' : ''}`,
         textContent: '+',
-        onclick: () => this.toggleVisibilty(),
+        onclick: () =>
+          isUpdatedMode ? this.props.onToggleHandler() : this.toggleVisibilty(),
       }),
       div(
         {
@@ -245,7 +304,9 @@ class AddNewTransaction extends Component<IProps, IState> {
           this.renderInputs(),
           button({
             className: 'add-new-btn',
-            textContent: '새로운 거래내역 등록하기',
+            textContent: isUpdatedMode
+              ? '현재 정보로 수정하기'
+              : '새로운 거래내역 등록하기',
             onclick: async () => await this.onAddHandler(),
           })
         )
